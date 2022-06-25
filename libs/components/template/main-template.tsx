0b79927095108c35b5b76c001b99337dfc612f6a
marc <marc@delacruz.com>
2022-06-26 02:11:16 +0800

    Improved SEO

--- libs/components/template/main-template.tsx
+++ libs/components/template/main-template.tsx
@@ -15,6 +15,7 @@ const MainTemplate = (props: MainTemplateProps) => {
         <title>Lester Dela Cruz</title>
         <meta name="description" content="Developer Portfolio - Lester" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="all" />
       </Head>
       <main className="bg-slate-900 h-screen">
         {children}
